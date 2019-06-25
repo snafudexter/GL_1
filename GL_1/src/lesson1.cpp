e4dffaa93e11f5c11939e59a24e032f89ffd1b0c
prabh <prabh@live.in>
2019-06-25 18:27:04 +0530

    updated code

--- GL_1/src/lesson1.cpp
+++ GL_1/src/lesson1.cpp
@@ -4,8 +4,51 @@
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 
+const char* APP_TITLE = "GL_1";
+const int gWindowWidth = 800;
+const int gWindowHeight = 600;
+
 int main()
 {
-	
+	if (!glfwInit())
+	{
+		std::cerr << "GLFW Init failed" << std::endl;
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow* pWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, APP_TITLE, NULL, NULL);
+
+	if (pWindow == NULL)
+	{
+		std::cerr << "Failed to create window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(pWindow);
+
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "Failed init glew" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+
+	//main loop
+	while (!glfwWindowShouldClose(pWindow)) {
+
+		glfwPollEvents();
+
+		glfwSwapBuffers(pWindow);
+	}
+
+	glfwTerminate();
 	return 0;
 }
