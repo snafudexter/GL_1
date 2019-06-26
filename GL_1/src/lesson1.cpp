d26ef3d319bb6ba52518a0dcf66f5d09a3fc2335
prabh <prabh@live.in>
2019-06-26 12:48:25 +0530

    fps and input

--- GL_1/src/lesson1.cpp
+++ GL_1/src/lesson1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <sstream>
 #define GLEW_STATIC
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
@@ -7,6 +7,9 @@
 const char* APP_TITLE = "GL_1";
 const int gWindowWidth = 800;
 const int gWindowHeight = 600;
+
+void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode);
+void showFPS(GLFWwindow* window);
 
 int main()
 {
@@ -31,6 +34,8 @@ int main()
 	}
 
 	glfwMakeContextCurrent(pWindow);
+	//glfwSwapInterval(0);
+	glfwSetKeyCallback(pWindow, glfw_onKey);
 
 	glewExperimental = GL_TRUE;
 	if (glewInit() != GLEW_OK)
@@ -44,6 +49,7 @@ int main()
 	//main loop
 	while (!glfwWindowShouldClose(pWindow)) {
 
+		showFPS(pWindow);
 		glfwPollEvents();
 
 		glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
@@ -54,4 +60,43 @@ int main()
 
 	glfwTerminate();
 	return 0;
+}
+
+void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+}
+
+void showFPS(GLFWwindow* window)
+{
+	static double prevSecs = 0.0;
+	static int frameCount = 0;
+
+	double elapsedSecs;
+	double currentSecs = glfwGetTime();
+
+	elapsedSecs = currentSecs - prevSecs;
+
+	if (elapsedSecs > 0.25)
+	{
+		prevSecs = currentSecs;
+		double fps = (double)frameCount / elapsedSecs;
+		double msPerFrame = 1000.0 / fps;
+
+		std::ostringstream outs;
+		outs.precision(3);
+
+		outs << std::fixed
+			<< APP_TITLE << "  "
+			<< "FPS: " << fps << "  "
+			<< "Frame Time: " << msPerFrame << "(ms)";
+		glfwSetWindowTitle(window, outs.str().c_str());
+
+		frameCount = 0;
+	}
+	
+	frameCount++;
+
 }
