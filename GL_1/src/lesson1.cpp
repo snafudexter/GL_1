d743c2f9bca6d2dec6676db531f6c83ff34ccd5c
prabh <prabh@live.in>
2019-06-26 14:50:38 +0530

    triangle interleaved buffer

--- GL_1/src/lesson1.cpp
+++ GL_1/src/lesson1.cpp
@@ -4,19 +4,144 @@
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 
-const char* APP_TITLE = "GL_1";
+const char* APP_TITLE = "First Triangle";
 const int gWindowWidth = 800;
 const int gWindowHeight = 600;
+
+GLFWwindow* gWindow = NULL;
+
+const GLchar* vertexShaderSrc =
+"#version 330 core\n"
+"layout (location = 0) in vec3 pos;\n"
+"layout (location = 1) in vec3 color;\n"
+"out vec3 vert_color;"
+"void main()"
+"{"
+" vert_color = color;"
+" gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);"
+"}"
+;
+
+const GLchar* fragmentShaderSrc =
+"#version 330 core\n"
+"in vec3 vert_color;"
+"out vec4 frag_color;"
+"void main()"
+"{"
+" frag_color = vec4(vert_color, 1.0f);"
+"}"
+;
+ 
 
 void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode);
 void showFPS(GLFWwindow* window);
 
+bool initOpenGL();
+
 int main()
+{
+	if (!initOpenGL())
+	{
+		std::cerr << "OpenGL init failed" << std::endl;
+	}
+
+
+	//TRIANGLE STUFF
+	GLfloat vertices[] = {
+		 0.0f,   0.5f,  0.0f, 1.0f, 0.0f, 0.0f,
+		 0.5f,  -0.5f,  0.0f, 0.0f, 1.0f, 0.0f,
+		-0.5f,  -0.5f,  0.0f, 0.0f, 0.0f, 1.0f
+	};
+
+
+
+	GLuint vbo, vao;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); 
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	//pos
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 6, NULL);
+	glEnableVertexAttribArray(0);
+
+	//color
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 6, (GLvoid*)(sizeof(GLfloat)*3));
+	glEnableVertexAttribArray(1);
+
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vertexShaderSrc, NULL);
+	glCompileShader(vs);
+
+	GLint result;
+	GLchar infoLog[512];
+	glGetShaderiv(vs, GL_COMPILE_STATUS, &result);
+
+	if (!result)
+	{
+		glGetShaderInfoLog(vs, sizeof(infoLog), NULL, infoLog);
+		std::cerr << "Error! vertex shader failed to compile. " << infoLog << std::endl;
+	}
+
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fragmentShaderSrc, NULL);
+	glCompileShader(fs);
+
+	glGetShaderiv(fs, GL_COMPILE_STATUS, &result);
+
+	if (!result)
+	{
+		glGetShaderInfoLog(fs, sizeof(infoLog), NULL, infoLog);
+		std::cerr << "Error! vertex shader failed to compile. " << infoLog << std::endl;
+	}
+
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vs);
+	glAttachShader(shaderProgram, fs);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &result);
+	if (!result)
+	{
+		glGetProgramInfoLog(shaderProgram, sizeof(infoLog), NULL, infoLog);
+		std::cerr << "Error! Shader program linker failure. " << infoLog << std::endl;
+	}
+
+	glDeleteShader(fs);
+	glDeleteShader(vs);
+
+	//main loop
+	while (!glfwWindowShouldClose(gWindow)) {
+
+		showFPS(gWindow);
+		glfwPollEvents();
+
+		glClear(GL_COLOR_BUFFER_BIT);
+		glBindVertexArray(vao);
+
+		glUseProgram(shaderProgram);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glBindVertexArray(0);
+		glfwSwapBuffers(gWindow);
+	}
+
+	glDeleteProgram(shaderProgram);
+	glDeleteVertexArrays(1, &vao);
+	glDeleteBuffers(1, &vbo);
+
+	glfwTerminate();
+	return 0;
+}
+
+bool initOpenGL()
 {
 	if (!glfwInit())
 	{
 		std::cerr << "GLFW Init failed" << std::endl;
-		return -1;
+		return false;
 	}
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -24,42 +149,31 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-	GLFWwindow* pWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, APP_TITLE, NULL, NULL);
+	gWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, APP_TITLE, NULL, NULL);
 
-	if (pWindow == NULL)
+	if (gWindow == NULL)
 	{
 		std::cerr << "Failed to create window" << std::endl;
 		glfwTerminate();
-		return -1;
+		return false;
 	}
 
-	glfwMakeContextCurrent(pWindow);
+	glfwMakeContextCurrent(gWindow);
 	//glfwSwapInterval(0);
-	glfwSetKeyCallback(pWindow, glfw_onKey);
+	glfwSetKeyCallback(gWindow, glfw_onKey);
 
 	glewExperimental = GL_TRUE;
 	if (glewInit() != GLEW_OK)
 	{
 		std::cerr << "Failed init glew" << std::endl;
 		glfwTerminate();
-		return -1;
+		return false;
 	}
 
+	//set clear color
+	glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
 
-	//main loop
-	while (!glfwWindowShouldClose(pWindow)) {
-
-		showFPS(pWindow);
-		glfwPollEvents();
-
-		glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glfwSwapBuffers(pWindow);
-	}
-
-	glfwTerminate();
-	return 0;
+	return true;
 }
 
 void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode) {
